3033cfb9fcbbcc3607c79edc8c2e6fea74285cf1
MAC <mac@MacBook-Pro.lan>
2023-04-06 18:29:45 -0400

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This assingment is working on for loops 
---
+++ for_loops_basic1.py
@@ -0,0 +1,34 @@
+for i in range(151):
+    print(i)
+
+for i in range(1000):
+    if i % 5 == 0:
+        print(i)
+
+def coding_dojo():
+    for i in range(1,101,1):
+        if i % 10 == 0:
+            print("Coding Dojo")
+        elif i % 5 == 0:
+            print("Coding")
+        else:
+            print(i)
+
+coding_dojo()
+
+odd_total = 0 
+
+for i in range(0, 500000, 2):
+    odd_total = odd_total + i
+
+print (odd_total)
+
+for i in range(2018,0,-4):
+    print(i)
+
+lowNum = 2
+highNum = 9
+mult = 3 
+for i in range(2,10):
+    if i % 3 == 0:
+        print(i)
